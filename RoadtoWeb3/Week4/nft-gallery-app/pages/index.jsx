3aae76d03d9de8345bfe5ea7dff75e2543e37da6
TheeSolomon <solomonwizardmon@gmail.com>
2022-05-30 06:58:12 +0800

    Adding Project Files

---
+++ RoadtoWeb3/Week4/nft-gallery-app/pages/index.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+const Home = () => {
+  const [wallet,setWalletAddress] = useState("");
+  const [collection,setCollectionAddress] = useState("");
+
+  return (
+    <div className="flex flex-col items-center justify-center py-8 gap-y-3">
+      <div className="flex flex-col w-full justify-center items-center gap-y-2">
+        <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} placeholder="Input wallet address."></input>
+        <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} placeholder="Input collection address."></input>
+        <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
+        <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
+          () => {
+            
+          }
+        }>Let's go!</button>
+      </div>
+    </div>
+  )
+}
+
+export default Home
