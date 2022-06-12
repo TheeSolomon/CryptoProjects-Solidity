d58e129b0a8e59fa6fddd7ed1d907b1eeddb7289
TheeSolomon <solomonwizardmon@gmail.com>
2022-06-13 03:07:34 +0800

    Final Construction of Code
    Added NFT Card component. Still has issues with NFT Card descriptions, but so far website is functioning.

--- RoadtoWeb3/Week4/nft-gallery-app/pages/index.jsx
+++ RoadtoWeb3/Week4/nft-gallery-app/pages/index.jsx
@@ -1,20 +1,79 @@
-import { useState } from 'react'
+import { NFTCard } from "../components/nftCard"
+import { redirect } from 'next/dist/server/api-utils';
+import { useState } from 'react' 
 
 const Home = () => {
   const [wallet,setWalletAddress] = useState("");
   const [collection,setCollectionAddress] = useState("");
+  const [NFTs, setNFTs] = useState([]);
+  const [fetchForCollection, setFetchForCollection] = useState(false);
+
+  const fetchNFTs = async() => {
+    let nfts;
+    console.log("Fetching NFTs...");
+    const api_key = "hcp6u6NmglCcse8AvwVGaPxcH8s5-bCR"
+    const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
+    var requestOptions = {
+      method: 'GET'
+    };
+    
+    if(!collection.length) {
+      
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+
+    } else {
+      console.log("Fetching NFTs for collection owned by address.")
+      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    }
+
+    if (nfts) {
+      console.log("nfts:", nfts)
+      setNFTs(nfts.ownedNfts)
+    }
+  } 
+
+  async function fetchNFTsForCollection() {
+      if(collection.length) {
+        var requestOptions = {
+          method: 'GET'
+        };
+        const api_key = "hcp6u6NmglCcse8AvwVGaPxcH8s5-bCR"
+        const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
+        const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${withMetadata}`;
+        const nfts =  await fetch(fetchURL, requestOptions).then(data => data.json())
+        if (nfts) {
+          console.log("NFTs in collection:", nfts)
+          setNFTs(nfts.nfts)
+      }
+    } 
+  }
 
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
         <input onChange={(e)=>{setWalletAddress(e.target.value)}} value={wallet} placeholder="Input wallet address."></input>
         <input onChange={(e)=>{setCollectionAddress(e.target.value)}} value={collection} placeholder="Input collection address."></input>
-        <label className="text-gray-600 "><input type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
+        <label className="text-gray-600 "><input onChange={(e)=>{setFetchForCollection(e.target.checked)}}type={"checkbox"} className="mr-2"></input>Fetch for Collection</label>
         <button className={"disabled:bg-slate-500 text-white bg-blue-400 px-4 py-2 mt-3 rounded-sm w-1/5"} onClick={
           () => {
-            
+            if (fetchForCollection) {
+              fetchNFTsForCollection()
+            } else {
+              fetchNFTs()
+            }
           }
         }>Let's go!</button>
+      </div>
+      <div className='flex flex-wrap gap-y-12 mt-4 w-5/6 gap-x-2 justify-center'>
+        {
+          NFTs.length && NFTs.map(nft => {
+            return (
+              <NFTCard nft={nft}></NFTCard>
+            )
+          })
+        }
       </div>
     </div>
   )
